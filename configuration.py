276459c3f95c9a655377170325be13b9c7c360c5
karic <katheph2015@gmail.com>
2025-05-10 17:04:11 -0500

    subiendo cambios 100525

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-a8cd8d8e-10be-44b2-8a3b-35c0e52902df.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-ea170541-cf07-42dc-b3bb-d1172c9f7900.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 PRODUCTS_KITS_PATH = "/api/v1/kits/"
